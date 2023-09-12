a4e52eed07ba6a0551ba64fb47d0c975a8b113df
Écio Ferraz <ecioferraz@gmail.com>
2023-09-11 21:04:15 -0300

    chore: radix ui

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,9 @@
-import './App.css'
+import { Button } from "./components/ui/button";
 
 export default function App() {
   return (
-    <h1>
+    <Button>
       Hello, World!
-    </h1>
+    </Button>
   )
 }
