5d102dba04d26729d9460b64168c351e8074b42f
Écio Ferraz <ecioferraz@gmail.com>
2023-09-13 20:20:41 -0300

    refactor: createTranscription

--- api/src/routes/create-transcription.ts
+++ api/src/routes/create-transcription.ts
@@ -1,17 +1,16 @@
 import { FastifyInstance } from 'fastify';
 import { createReadStream } from 'fs';
 import { z } from 'zod';
+import openai from '../lib/openai';
 import prisma from '../lib/prisma';
 
 export default async function createTranscription(app: FastifyInstance) {
-  app.post('/videos/:videoId/transcription', async (req, res) => {
+  app.post('/videos/:videoId/transcription', async (req) => {
     const paramsSchema = z.object({ videoId: z.string().uuid() });
 
     const { videoId } = paramsSchema.parse(req.params);
 
-    const bodySchema = z.object({
-      prompt: z.string(),
-    });
+    const bodySchema = z.object({ prompt: z.string() });
 
     const { prompt } = bodySchema.parse(req.body);
 
@@ -21,12 +20,22 @@ export default async function createTranscription(app: FastifyInstance) {
 
     const videoPath = video.path;
 
-    const audioReadStream = createReadStream(videoPath);
+    const file = createReadStream(videoPath);
 
-    return {
+    const { text: transcription } = await openai.audio.transcriptions.create({
+      file,
+      language: 'pt',
+      model: 'whisper-1',
       prompt,
-      videoId,
-      videoPath,
-    };
+      response_format: 'json',
+      temperature: 0,
+    });
+
+    await prisma.video.update({
+      where: { id: videoId },
+      data: { transcription },
+    });
+
+    return { transcription };
   });
 }
