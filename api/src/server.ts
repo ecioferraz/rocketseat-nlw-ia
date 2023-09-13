06e53610ef5d3b34a499ee1ddab273b5da8aabb3
Écio Ferraz <ecioferraz@gmail.com>
2023-09-13 20:23:58 -0300

    chore: fastify/cors

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import createTranscription from './routes/create-transcription';
 import generateAICompletion from './routes/generate-ai-completion';
 import getAllPrompts from './routes/get-all-prompts';
 import uploadVideo from './routes/upload-video';
 
 const app = fastify();
+
+app.register(fastifyCors, { origin: '*' });
 
 app.register(getAllPrompts);
 app.register(uploadVideo);
